2cdf542379b80615b39c7051a5a916b2ec052cc8
Celine Langeland <celinelangeland8@gmail.com>
2025-05-03 12:19:24 +0200

    Final cleaning of code

--- src/retrieval.py
+++ src/retrieval.py
@@ -1,6 +1,5 @@
 import os
 import fitz  
-import pdfplumber
 import pandas as pd
 
 # define project root directory
@@ -14,18 +13,4 @@ def extract_text_from_pdf(pdf_path):
     doc = fitz.open(pdf_path)
     text_data = [{"page": i + 1, "content": page.get_text("text")} for i, page in enumerate(doc)]
     return pd.DataFrame(text_data)
-
-'''
-def extract_tables_from_pdf(pdf_path):
-    """Extracts tables from a PDF and returns a DataFrame."""
-    tables_data = []
-    with pdfplumber.open(pdf_path) as pdf:
-        for i, page in enumerate(pdf.pages):
-            tables = page.extract_tables()
-            for table in tables:
-                df_table = pd.DataFrame(table)
-                df_table["page"] = i + 1
-                tables_data.append(df_table)
-    return pd.concat(tables_data, ignore_index=True) if tables_data else pd.DataFrame()
-'''
 
